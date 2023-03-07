060e2d0cd01c890fe57b5418fd543294c193f009
Sebas <122642717+Sebas2D0@users.noreply.github.com>
2023-03-07 00:22:52 -0300

    Create funciones.js

---
+++ funciones.js
@@ -0,0 +1,9 @@
+
+function mostraronofuncion() {
+    var x = document.getElementById("referencias");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
